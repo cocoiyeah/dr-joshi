9df75c285bc16dc2723ba939a7320123b9cf9359
kadek ramsyana <kadek.ramsyana@karmagroup.com>
2017-12-01 13:29:08 +0800

    first 01/12

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Under Construction</title>
+  <meta name="description" content="The underconstruction page">
+  <meta name="author" content="Under Construction">
+  <!-- <link rel="stylesheet" href="css/styles.css?v=1.0"> -->
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<p>Under Construction</p>
+<!-- <script src="js/scripts.js"></script> -->
+</body>
+</html>
